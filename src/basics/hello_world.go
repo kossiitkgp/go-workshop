f2f02c29cb45fe8301333f9ee428eb0ffcc8bc4c
Kshitij Saraogi <KshitijSaraogi@gmail.com>
2018-01-19 17:20:06 +0530

    Added Hello World example

---
+++ src/basics/hello_world.go
@@ -0,0 +1,12 @@
+package main
+
+// Go programs are made up of packages
+
+import "fmt"
+
+// Programs start running in package main
+func main() {
+	fmt.Println("Hello, World!")
+	// Strings in Go are Unicode compliant and are UTF-8 Encoded.
+	fmt.Println("Hello, वर्ल्ड!")
+}
